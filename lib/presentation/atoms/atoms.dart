ce3b80d5c6f1bb24e967aac54bb5d683ca0f4605
Guy Luz <guyluz11@gmail.com>
2024-11-28 17:12:53 +0200

    Added splash screen

--- lib/presentation/atoms/atoms.dart
+++ lib/presentation/atoms/atoms.dart
@@ -1,6 +1,7 @@
 export 'package:flutter_app_template_atoms/presentation/atoms/button_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/card_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/check_box_atom.dart';
+export 'package:flutter_app_template_atoms/presentation/atoms/image_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/list_tile_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/margined_expanded_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/separator_atom.dart';
--- lib/presentation/pages/pages.dart
+++ lib/presentation/pages/pages.dart
@@ -1,1 +1,1 @@
-
+export 'package:flutter_app_template_atoms/presentation/pages/splash_screen_page.dart';
