aefee3580e60370202c40e9b0a3e57a8aa3b5e09
Guy Luz <guyluz11@gmail.com>
2024-12-01 16:15:00 +0200

    Added popup menu entry atom

--- lib/presentation/atoms/atoms.dart
+++ lib/presentation/atoms/atoms.dart
@@ -4,6 +4,7 @@ export 'package:flutter_app_template_atoms/presentation/atoms/check_box_atom.dar
 export 'package:flutter_app_template_atoms/presentation/atoms/image_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/list_tile_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/margined_expanded_atom.dart';
+export 'package:flutter_app_template_atoms/presentation/atoms/popup_menu_entry_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/separator_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/text_atom.dart';
 export 'package:flutter_app_template_atoms/presentation/atoms/toggle_switch_atom.dart';
